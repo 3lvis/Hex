6b23ba07e8821f7dec038ecc89006587121eb3e8
Elvis Nuñez <elvisnunez@me.com>
2014-04-25 08:58:20 +0200

    Initial commit

---
+++ UIColor-ANDYHex/UIColor+ANDYHex.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+ANDYHex.h
+//
+//  Copyright (c) 2014 Hyper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ANDYHex)
+
++ (UIColor *)colorFromHexString:(NSString *)hexString;
+
+@end
