740177d7044d9d85c2535789d84a09c4e7434957
Elvis Nuñez <3lvis@users.noreply.github.com>
2015-06-07 23:01:53 +0200

    Format code

--- Hex/UIColor+Hex.swift
+++ Hex/UIColor+Hex.swift
@@ -15,13 +15,17 @@ extension UIColor {
             let blue = (hexInt) & 0xFF
 
             self.init(
-                red: CGFloat(red) / 255,
-                green: CGFloat(green) / 255,
-                blue: CGFloat(blue) / 255,
-                alpha: 1
+                red: CGFloat(red) / 255.0,
+                green: CGFloat(green) / 255.0,
+                blue: CGFloat(blue) / 255.0,
+                alpha: 1.0
             )
         } else {
-            self.init(red: 0, green: 0, blue: 0, alpha: 0)
+            self.init(
+                red: 0.0,
+                green: 0.0,
+                blue: 0.0,
+                alpha: 0.0)
         }
     }
 }
