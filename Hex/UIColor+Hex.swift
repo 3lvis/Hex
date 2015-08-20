5e5f69685b6128cd041644f7b7c3eccf96526975
Elvis Nuñez <3lvis@users.noreply.github.com>
2015-08-20 12:31:35 +0200

    Update UIColor+Hex.swift

--- Hex/UIColor+Hex.swift
+++ Hex/UIColor+Hex.swift
@@ -2,9 +2,7 @@ import UIKit
 
 extension UIColor {
     public convenience init(hex: String) {
-        let noHasString = hex.stringByReplacingOccurrencesOfString(
-            "#",
-            withString: "")
+        let noHasString = hex.stringByReplacingOccurrencesOfString("#", withString: "")
         let scanner = NSScanner(string: noHasString)
         scanner.charactersToBeSkipped = NSCharacterSet.symbolCharacterSet()
 
@@ -14,18 +12,9 @@ extension UIColor {
             let green = (hexInt >> 8) & 0xFF
             let blue = (hexInt) & 0xFF
 
-            self.init(
-                red: CGFloat(red) / 255.0,
-                green: CGFloat(green) / 255.0,
-                blue: CGFloat(blue) / 255.0,
-                alpha: 1.0
-            )
+            self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
         } else {
-            self.init(
-                red: 0.0,
-                green: 0.0,
-                blue: 0.0,
-                alpha: 0.0)
+            self.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
         }
     }
 
@@ -33,3 +22,5 @@ extension UIColor {
         return UIColor(hex: hex)
     }
 }
+
+let color = UIColor(hex: "FFFFFF")
